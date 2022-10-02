1c128c8d3152f9c3a035b04892e3a261227c3ea2
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-02 13:34:44 -0700

    moved to dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import { Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
 
+      </Router>
     </div>
   );
 }
